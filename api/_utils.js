50f4e3096fd054515242961c3cee1913e2e55a8e
l76000 <colakovicl035@gmail.com>
2025-11-09 10:22:51 +0100

    Update _utils.js

--- api/_utils.js
+++ api/_utils.js
@@ -352,8 +352,7 @@ if (!tripId || !vehicleId) {
       const directionPrefix = parts[0];
       const timeShort = parts[1];
       const timeFull = `${timeShort.slice(0, 2)}:${timeShort.slice(2)}:00`;
-     
-console.log(`Pokušavam poklapanje. Smer: ${directionPrefix}. Vreme iz Trip ID-a: ${timeFull}`);
+    
      
       let mapToUse;
       if (directionPrefix === '8170') {
